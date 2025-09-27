498c88732d3315d817d8c54b0e07bdd17a16b497
Adis Durakovic <git@mail.adisdurakovic.com>
2025-09-27 10:12:08 +0200

    default json errors

--- apps/backend/app/Middleware/JsonError.ts
+++ apps/backend/app/Middleware/JsonError.ts
@@ -1,7 +1,15 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class JsonError {
-  public async handle({}: HttpContextContract, next: () => Promise<void>) {
+  public async handle({ response }: HttpContextContract, next: () => Promise<void>) {
     await next()
+    const code = response.response.statusCode
+    const body = response.getBody()
+    if (code >= 400) {
+      response.json({
+        error: body,
+        code: code,
+      })
+    }
   }
 }
